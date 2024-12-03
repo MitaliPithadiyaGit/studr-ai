74679f54f25fd8a30574e4e5c92464cd001ca75f
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 17:39:33 +0530

    build file css link

--- app/root.tsx
+++ app/root.tsx
@@ -66,6 +66,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="/build/_assets/tailwind-FOUZUSUO.css"
+        />
         <Meta />
         <Links />
       </head>
