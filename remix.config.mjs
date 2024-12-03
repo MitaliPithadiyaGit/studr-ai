5b5f9c0040ca046026919f9d47960517c6399a9a
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 14:59:21 +0530

    new file add

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,6 +1,9 @@
 export default {
-  serverBuildTarget: "vercel",
-  serverBuildPath: "build/index.js", // Ensure build path points outside 'public'
-  publicPath: "/public/build/",       // Static files location
-  appDirectory: "app",
+  serverBuildTarget: 'vercel',
+  server: './server.js',
+  appDirectory: 'app',
+  assetsBuildDirectory: '/build', // Ensure this is correct
+  publicPath: '/build/', // Ensure this is correct
+  serverBuildPath: 'public/build/', // Path to the server build
+  devServerPort: 8002,
 };
