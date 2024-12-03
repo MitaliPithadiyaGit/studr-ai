f0f71ef928d4713f8e530726a1d760896922d619
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 16:53:32 +0530

    issue fixed

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-import { createRequestHandler } from '@remix-run/node'; // Remix runtime for handling requests
-import * as build from './build/index.js'; // Explicitly import the index.js file from the build folder
+import { createRequestHandler } from '@remix-run/vercel';
+import * as build from '@remix-run/dev/server-build';
 
-export default createRequestHandler({
-  build,
-  getLoadContext() {
-    // Optionally add context for things like session or database
-  },
-});
+export default createRequestHandler({ build });
