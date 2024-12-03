9fe59fb5bca9e783c64fc934ef55ba80b330f6a7
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 16:30:24 +0530

    some new file chnage issue fix

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-import { createRequestHandler } from '@remix-run/vercel';
+import { createRequestHandler } from '@remix-run/node'; // For server-side routing in Remix
+import * as build from './build'; // Make sure this path points to your build folder
 
-export default createRequestHandler();
+export default createRequestHandler({
+  build,
+  getLoadContext() {
+    // Optionally, you can add context here (e.g., session data, authentication, etc.)
+  },
+});
