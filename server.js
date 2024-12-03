a4f34ed0ced3867857f29db72bb41584acc546ae
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 17:05:18 +0530

    vercel json file some new

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import build from './build/index.js';  // Import default export from index.js
+import * as build from './build/index.js';// Import default export from index.js
 import { createRequestHandler } from '@remix-run/vercel';
 
 export default createRequestHandler({ build });
