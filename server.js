83457cb7e58b9a20e3b56014d83e9d1cc421ebbc
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 13:06:31 +0530

    server file

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-import { createRequestHandler } from "@remix-run/vercel";
-import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from '@remix-run/vercel';
+import * as build from './public/build';  // Ensure the correct path to the build folder
 
-export default createRequestHandler({ build });
+export default function handleRequest(req, res) {
+  return createRequestHandler({ build, req, res });
+}
