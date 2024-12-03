27cb89c6f438cddb9f06f8519135cf339ae489b2
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-03 17:01:40 +0530

    vercel json file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { createRequestHandler } from '@remix-run/node';
-import { build } from './build';  // Your build artifacts should be imported here
+import { build } from './build/index.js';  // Your build artifacts should be imported here
 
 export default function handler(req, res) {
   return createRequestHandler({ build })(req, res);
