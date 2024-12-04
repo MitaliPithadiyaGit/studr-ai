5320af0dc36e1960d4c54f803fabc585e9f40a8b
MitaliPithadiyaGit <mitalipithadiya26@gmail.com>
2024-12-04 16:47:45 +0530

    build chnages fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: [  './app/**/*.{js,jsx,ts,tsx}', // Ensure app folder content is included
+    './pages/**/*.{js,jsx,ts,tsx}', // If you also have any pages directory
+    './components/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {},
   },
